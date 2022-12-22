4f7846eb732320383a9eed225de20f05cd33f2d1
eazhuravlevawk <121089866+eazhuravlevawk@users.noreply.github.com>
2022-12-22 11:33:21 +0300

    Add files via upload

---
+++ skybox/js/script.js
@@ -0,0 +1,9 @@
+const slider = tns({
+    container: '.carousel__wrapper',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    autoplay: true,
+    autoplayTimeout: 2800
+  });
